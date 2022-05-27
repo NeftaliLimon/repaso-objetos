2465363fb1ca3f88b9afccf361ed3e2cb4a7784e
NeftaliLimon <nef.nl66@gmail.com>
2022-05-26 23:22:01 -0500

    Tiempo: 20 min

--- src/main/java/uaslp/objetos/figuras/Cuadrado.java
+++ src/main/java/uaslp/objetos/figuras/Cuadrado.java
@@ -3,8 +3,26 @@ package uaslp.objetos.figuras;
 public class Cuadrado {
 
     private double lado;
+    private double area;
+
+    public Cuadrado(){
+
+    }
 
     void setLado(double lado){
         this.lado = lado;
+    }
+
+    public double getArea() {
+        area=lado*lado;
+        return 0;
+    }
+
+    public Cuadrado(double lado){
+        this.lado=lado;
+    }
+
+    public double getLado() {
+        return lado;
     }
 }
---
+++ src/main/java/uaslp/objetos/figuras/PoligonoRegular.java
@@ -0,0 +1,38 @@
+package uaslp.objetos.figuras;
+
+public class PoligonoRegular {
+    private double lado;
+    private double area;
+    private int numeroDeLados;
+
+    public void setLado(double lado) {
+        this.lado = lado;
+    }
+
+    public double getArea() {
+        double perimetro;
+        double apotema;
+        double anguloCentral;
+        perimetro=numeroDeLados*lado;
+        anguloCentral=360/numeroDeLados;
+        apotema=lado/(2*Math.tan(Math.toRadians(anguloCentral/2)));
+        area=(perimetro*apotema)/2;
+        return area;
+    }
+
+    public double getLado() {
+        return lado;
+    }
+
+    public PoligonoRegular(int numeroDeLados, double lado){
+
+        this.numeroDeLados=numeroDeLados;
+        this.lado=lado;
+    }
+
+    public PoligonoRegular(int numeroDeLados){
+        this.numeroDeLados=numeroDeLados;
+    }
+
+}
+
